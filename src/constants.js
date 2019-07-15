35e9eb13457e06ee684ee232ba3e6f7a95808806
borkjt9 <borkjt9@gmail.com>
2019-07-14 23:57:15 -0400

    Map Firestore's response to Redux

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,10 @@
 // You can literally assign constants here to any values.
 // It doesn't matter as long as the references make sense.
 
-export const FETCH_ANSWER = 'FETCH_ANSWER';
+// Action Types. Feed into the reducer.
+export const FETCH_ANSWER_START = 'FETCH_ANSWER_START';
+export const FETCH_ANSWER_FULFILLED = 'FETCH_ANSWER_FULFILLED';
+export const FETCH_ANSWER_ERRORED = 'FETCH_ANSWER_ERRORED';
 
 // Statuses of requests to Firestore. Feed into the Reducer.
 export const STATUS_PENDING = 'STATUS_PENDING';
--- src/redux/actions/fetch-answer.js
+++ src/redux/actions/fetch-answer.js
@@ -1,15 +1,51 @@
+// Use constants to reduce fat finger bugs.
 import {
-  FETCH_ANSWER
-} from '../../constants';
+  FETCH_ANSWER_START,
+  FETCH_ANSWER_FULFILLED,
+  FETCH_ANSWER_ERRORED,
+  STATUS_PENDING,
+  STATUS_ERROR,
+  STATUS_SUCCESS
+  } from '../../constants';
+
+// Start, Fulfilled, and Error Actions all report on the status of a request.
+// They are called during the request to Firestore.
+// They feed into the Answer Reducer.
 
 /**
- * fetchAnswer
+ * fetchAnswerStart
  *
  * @return {any}      feeds into reducer
  */
-export default function fetchAnswer() {
+export function fetchAnswerStart() {
   return {
-    type: FETCH_ANSWER,
-    payload: 'Hey! This is a placeholder.',
+    type: FETCH_ANSWER_START,
+    payload: {status: STATUS_PENDING},
+  };
+}
+
+/**
+ * fetchAnswerFulfilled
+ *
+ * @param  {Object} response
+ * @return {any}      feeds into reducer
+ */
+export function fetchAnswerFulfilled(response) {
+  return {
+    type: FETCH_ANSWER_FULFILLED,
+    payload: { status: STATUS_SUCCESS, response },
+  };
+}
+
+/**
+ * fetchActiveSitesErrored
+ *
+ * @param  {Error} error code & message
+ * @return {any}      feeds into reducer
+ */
+export function fetchAnswerErrored(error) {
+  return {
+    type: FETCH_ANSWER_ERRORED,
+    payload: { status: STATUS_ERROR, error } ,
   };
 }
